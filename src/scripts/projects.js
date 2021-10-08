d71854e5d1bb10af4ad0aa43acd29ce610cc2bf8
John Carlo Menorias <47134598+CarloStokMnl@users.noreply.github.com>
2021-10-08 19:44:43 +0800

    Adding truncate and image to jordan

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,6 +1,6 @@
 const projectList = {
 	init() {
-		this.processArray(processListData);
+		this.processArray(projectListData);
 	},
 	processArray(data) {
 		const projectData = data;
@@ -8,7 +8,7 @@ const projectList = {
 
 		Object.keys(projectData).forEach(function(key) {
 			projectCardContainer.insertAdjacentHTML('beforeend', `
-				<div class="col-lg-4">
+				<div class="col-12 col-md-6 col-lg-4">
 					<div id="project-item-${projectData[key].id}" class="project-card" 
 					data-name="${projectData[key].projectName}" 
 					data-id="${projectData[key].id}">
@@ -18,24 +18,35 @@ const projectList = {
 							</figure>
 						</a>
 						<h3 class="project-name">${projectData[key].projectName}</h3>
-						<p class="project-description">${projectData[key].projectDescription}</p>
+						<p class="project-description js-project-description">${projectData[key].projectDescription}</p>
 					</div>
 				</div>
 			`);
+
+			if(key == projectData.length - 1) {
+				projectList.truncateData();
+			}
+		});
+	},
+	truncateData() {
+		new Cuttr('.js-project-description', {
+			//options here
+			truncate: 'words',
+			length: 16
 		});
 	}
 }
 
-const processListData = [{
+const projectListData = [{
 	"id": 1,
-	"projectName": "Nutrilett.se",
+	"projectName": "Nutrilett SE",
 	"projectDescription": "Nutrilett is a proven effective method for weight loss and contains all the necessary vitamins, proteins, minerals that the body needs",
 	"projectLink": "https://www.nutrilett.se/",
 	"projectYear": "2020",
 	"projectImage": "nutrilett-se.png"
 }, {
 	"id": 2,
-	"projectName": "Nutrilett.no",
+	"projectName": "Nutrilett NO",
 	"projectDescription": "Nutrilett is a proven effective method for weight loss and contains all the necessary vitamins, proteins, minerals that the body needs",
 	"projectLink": "https://www.nutrilett.no/",
 	"projectYear": "2021",
@@ -54,4 +65,12 @@ const processListData = [{
 	"projectLink": "https://www.mollers.no/",
 	"projectYear": "2021",
 	"projectImage": "mollers-no.png"
-}]
+}, {
+	"id": 5,
+	"projectName": "Jordan",
+	"projectDescription": "Jordan is a Scandinavian brand that has been caring for people’s teeth since 1927. Our range of high quality, easy-to-use and stylish products are designed for every stage of your life, making sure you will find one that fits you.	",
+	"projectLink": "https://www.jordanoralcare.com/",
+	"projectYear": "2020",
+	"projectImage": "jordan.png"
+}
+]
