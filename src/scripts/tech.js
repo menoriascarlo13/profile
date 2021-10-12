3ff9f293b864ebb65fdd5b9e3b1a014f197c2a55
John Carlo Menorias <47134598+CarloStokMnl@users.noreply.github.com>
2021-10-12 21:31:22 +0800

    Adding popper

--- src/scripts/tech.js
+++ src/scripts/tech.js
@@ -8,9 +8,7 @@ const tech = {
 
 		Object.keys(techData).forEach(function(key) {
 			techCardContainer.insertAdjacentHTML('beforeend', `
-				<div id="tech-item-${techData[key].id}" class="tech-item" data-techId="${techData[key].id}" data-techName="${techData[key].techName}"
-					data-toggle="tooltip" data-placement="bottom" title="${techData[key].techName}"
-				>
+				<div id="tech-item-${techData[key].id}" class="tech-item" data-techId="${techData[key].id}" data-techName="${techData[key].techName}">
 					<figure>
 						<img src="dist/images/images/${techData[key].techImage}" alt="${techData[key].techName}">
 					</figure>
