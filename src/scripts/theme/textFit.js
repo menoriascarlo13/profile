23479226e95d01403e0194df2b24e8cadf5f945e
John Carlo Menorias <47134598+CarloStokMnl@users.noreply.github.com>
2021-10-15 19:25:17 +0800

    removing console log on textfit.js

--- src/scripts/theme/textFit.js
+++ src/scripts/theme/textFit.js
@@ -1,8 +1,6 @@
 const txtFit = {
 	init(e, option) {
-		console.log(option);
 		if(option != null || option != undefined) {
-			console.log(option);
 			textFit(document.getElementsByClassName(e), option);
 		}
 	}
