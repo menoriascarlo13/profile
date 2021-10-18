78809bf4e1a8940aa98d85443692efafb4877486
John Carlo Menorias <47134598+CarloStokMnl@users.noreply.github.com>
2021-10-18 14:38:13 +0800

    Updating scroll nav

--- src/scripts/theme/nav.js
+++ src/scripts/theme/nav.js
@@ -4,14 +4,14 @@ const nav = {
 		const header = document.getElementById('js-header');
 		let navbarHeight;
 		try {
-			if(navbar == null) {
+			if (navbar == null) {
 				throw `nav.js: Missing Navigation Id / Element. Setting padding top of body to default. Removing fixed position setting of Navigation bar`;
 			}
 
-			if(header == null) {
+			if (header == null) {
 				throw `nav.js: Missing Header Id / Element. Setting padding top of body to default. Removing fixed position setting of Header bar`;
 			}
-	
+
 			navbarHeight = navbar.offsetHeight;
 			document.body.style.paddingTop = `${navbarHeight}px`;
 			navbar.classList.add('is-fixed');
@@ -27,22 +27,44 @@ const nav = {
 		const navItem = document.getElementsByClassName('js-nav-link');
 
 		try {
-			if(navItem.length == 0) {
+			if (navItem.length == 0) {
 				throw `nav.js: Missing Navigation Item Class / Element.`;
 			}
 
 			for (let i = 0; i < navItem.length; i++) {
-				navItem[i].addEventListener('click', function(e) {
-					nav.tabTitle(e);
-				})
-			 }
+				navItem[i].addEventListener('click', function (e) {
+					nav.changeTabTitle(e);
+					nav.scrollToSection(e);
+				});
+			}
 
 		} catch (error) {
 			console.error(error);
 			showError.init();
 		}
 	},
-	tabTitle(e) {
-		document.title = `JCPM - ${e.target.innerText}`;
+	changeTabTitle(element) {
+		document.title = `JCPM - ${element.target.innerText}`;
+	},
+	scrollToSection(element) {
+		const currElement = document.getElementById(element.target.dataset.nav);
+		const navbar = document.getElementById('js-navbar');
+
+		try {
+			if (currElement == null) {
+				throw `nav.js: Missing Target Navigation Item Class / Element.`
+			}
+
+			if (navbar == null) {
+				throw `nav.js: Missing Navigation Id / Element. Scrolling to section might not be exact or might not work`;
+			}
+
+			const finalPosition = currElement.offsetTop - navbar.offsetHeight;
+
+			window.scrollTo(0, finalPosition);
+			console.log(navbar.offsetHeight);
+		} catch (error) {
+			console.warn(error);
+		}
 	}
 }
